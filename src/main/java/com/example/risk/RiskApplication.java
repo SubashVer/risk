085f2546cf089956035bdbcf7f1fedcc179a45a8
SubashVer <144439558+SubashVer@users.noreply.github.com>
2023-11-28 00:22:24 +0530

    Update RiskApplication.java

--- src/main/java/com/example/risk/RiskApplication.java
+++ src/main/java/com/example/risk/RiskApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RiskApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RiskApplication.class, args);
-	}
+        public static void main(String[] args) {
+                SpringApplication.run(RiskApplication.class, args);
+        }
 
 }
